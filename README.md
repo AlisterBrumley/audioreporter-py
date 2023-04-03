732789e92294de1b558e91db584de16362c9feb9
Alister Brumley <109202180+AstaBrum@users.noreply.github.com>
2023-04-04 06:16:25 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# audioreporter-py
+
+Simple python script that takes command line filepath arguments and reports the bitrate-sample rate of the file.
+
+Useful for quickly identifying audio file details during projects, or in a folder of many similar files.
+
+Dependencies
+ - Python 3
+ - Pydub
+ - ffprobe (from ffmpeg)
---
+++ reporter.py
@@ -0,0 +1,27 @@
+import sys
+from pydub import AudioSegment
+
+# checks if argument is present quits otherwise
+if len(sys.argv) < 2:
+    print("needs a file")
+    exit()
+elif len(sys.argv) > 2:
+    print("too many args")
+    exit()
+
+
+file_path = sys.argv[1]
+
+# opens file
+try:
+    audio_file = AudioSegment.from_file(file_path)
+except Exception:
+    print("invalid file")
+    exit()
+
+# prints file details
+sr = str(audio_file.frame_rate)
+br = str(audio_file.sample_width * 8)
+print(sr + "hz - sample rate ")
+print(br + "bit - bit rate")
+# TODO return the encoding type
